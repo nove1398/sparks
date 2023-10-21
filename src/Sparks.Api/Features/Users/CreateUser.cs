10d72b03a770452e7f0f0794a12ecd51af19d747
Nove developer <nove1398@hotmail.com>
2023-10-21 18:01:51 -0500

    Update

--- src/Sparks.Api/Features/Users/CreateUser.cs
+++ src/Sparks.Api/Features/Users/CreateUser.cs
@@ -1,5 +1,6 @@
 using Carter;
 using MediatR;
+using Sparks.Api.Database;
 using Sparks.Api.Shared;
 using Supabase.Gotrue;
 
@@ -8,25 +9,54 @@ namespace Sparks.Api.Features.Users;
 public sealed class CreateUserCommand : ICommand<Result<Session?>>
 {
     public GenderTypes Gender { get; init; }
+    public string? Country { get; init; }
+    public int? Age { get; init; }
+    public int? HeightInCm { get; init; }
+    public StarSignTypes? StarSign { get; init; }
+    public required string Email { get; init; }
+    public required string Password { get; init; }
 }
 
 public sealed class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, Result<Session?>>
 {
     private readonly SupabaseClient.SupabaseClient _supabaseClient;
     private readonly ILogger<CreateUserCommandHandler> _logger;
-    public CreateUserCommandHandler(ILogger<CreateUserCommandHandler> logger, SupabaseClient.SupabaseClient supabaseClient)
+    private readonly SparksDbContext _dbContext;
+    public CreateUserCommandHandler(ILogger<CreateUserCommandHandler> logger, SupabaseClient.SupabaseClient supabaseClient, SparksDbContext dbContext)
     {
         _logger = logger;
         _supabaseClient = supabaseClient;
+        _dbContext = dbContext;
     }
     
     public async Task<Result<Session?>> Handle(CreateUserCommand request, CancellationToken cancellationToken)
     {
-        Session? createdSession = await _supabaseClient.CreateUserAccount("nove1398@yahoo.com","mypassword", new Dictionary<string, object>()
+        Session? createdSession = await _supabaseClient.CreateUserAccount(
+            request.Email,
+            request.Password, 
+            new Dictionary<string, object>()
         {
-            {"location", "Jamaica"}
+            {"location", request.Country}
         });
+        if (createdSession is null)
+        {
+            return Result<Session?>.Failure(new ErrorModel("Failed to register user account","Null response from server"));
+        }
+
+        if (createdSession.AccessToken is null)
+        {
+            return Result<Session?>.Failure(new ErrorModel("Account already created"));
+        }
+        
         // Create local user 
+        User.Create(
+            Guid.Parse(createdSession.User?.Id),
+            createdSession.User.Email!,
+            "Jamaica",
+            request.Age,
+            request.HeightInCm,
+            request.Gender,
+            request.StarSign);
         return Result<Session?>.Success(createdSession);
     }
 }
@@ -45,7 +75,13 @@ public sealed class CreateUserModule : ICarterModule
         {
             var createdSession = await mediator.Send(new CreateUserCommand()
             {
-                Gender = GenderTypes.Male
+                Gender = GenderTypes.Male,
+                Email = "mytestEmail@yahoo.com",
+                Password = "mypassword",
+                StarSign = StarSignTypes.Aquarius,
+                Age = 21,
+                Country = "Jamaica",
+                HeightInCm = 120
             });
             return Results.Ok(createdSession);
         });
